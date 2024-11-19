c609ffec726a7cb369f4594a21e171093053ff3e
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2024-11-19 16:26:55 +0100

    add solution

--- 03_feladat_1.py
+++ 03_feladat_1.py
@@ -1,16 +1,23 @@
 """
-1.1 Feladat
+1.1/2 Feladat
 Készíts egy programot, amely a felhasználó által megadott mondatról a mondatvégi jel alapján eldönti milyen fajtájú!
 (kijelentő, kérdő, felkiáltó / felszólító / óhatjtó)
 """
-
+folytat = True
 megadott_mondat = input("Adj meg egy mondatot!")
-
-if megadott_mondat[-1] == ".":
-    print("Ez a mondat kijelentő.")
-elif megadott_mondat[-1] == "?":
-    print("Ez a mondat kérdő.")
-elif megadott_mondat[-1] == "!":
-    print("Ez a mondat felkiáltó/felszótító/óhajtó.")
-else:
-    print("Nem adtál meg mondatvégi írásjelet!")
+while folytat:
+    if megadott_mondat == "":
+        folytat = False
+        print("program vége")
+    elif megadott_mondat[-1] == ".":
+        print("Ez a mondat kijelentő.")
+        megadott_mondat = input("Adj meg egy mondatot!")
+    elif megadott_mondat[-1] == "?":
+        print("Ez a mondat kérdő.")
+        megadott_mondat = input("Adj meg egy mondatot!")
+    elif megadott_mondat[-1] == "!":
+        print("Ez a mondat felkiáltó/felszótító/óhajtó.")
+        megadott_mondat = input("Adj meg egy mondatot!")
+    else:
+        print("Nem adtál meg mondatvégi írásjelet!")
+        megadott_mondat = input("Adj meg egy mondatot!")
